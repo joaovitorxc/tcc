e9afa222e19321020a52879e23543ff3cb2c25e0
joaovitorxc <jaovitorxc@gmail.com>
2025-05-15 15:39:16 -0300

    TCC atualizado

--- tcc_voice-main/tcc_voice-main/voice/pagina1.js
+++ tcc_voice-main/tcc_voice-main/voice/pagina1.js
@@ -1,14 +1,14 @@
 function mostrarPagina(id) {
-    // Verifica se os elementos existem antes de tentar acessá-los
+    // verifica se os elementos existem antes de tentar acessa-los
     var pagina1 = document.getElementById('pagina1');
     var pagina2 = document.getElementById('pagina2');
 
     if (pagina1 && pagina2) {
-        // Oculta todas as páginas
+        // oculta todas as páginas
         pagina1.style.display = 'none';
         pagina2.style.display = 'none';
 
-        // Mostra a página selecionada
+        // mostra a página selecionada
         var paginaSelecionada = document.getElementById(id);
         if (paginaSelecionada) {
             paginaSelecionada.style.display = 'block';
